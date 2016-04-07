700bdaf22d9b217e9d4019365ebbc906a0a15b83
Winnie <winniedinh89@gmail.com>
2016-04-07 15:05:43 -0700

    added menu animation

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var main = function(){
+
 	$('.dropdown-toggle').click(function(){
 		$('.dropdown-menu').toggle();
 	});
 
+    $('.icon-menu').click(function(){
+        $('.menu').animate({left: '0px'}, 200);
+        $('body').animate({left: '285px'}, 200);
+    });
+    
+    $('.icon-close').click(function(){
+        $('.menu').animate({left: '-285px'}, 200);
+        $('body').animate({left: '0px'}, 200);
+    });
 }
 
 $(document).ready(main);
